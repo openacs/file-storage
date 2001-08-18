202c2c08ed1361e54d5da93b4683f5c9ad0c25e5
Jowell Sabino <jowellsabino@netscape.net>
2001-08-18 15:52:02 +0000

    Added dm_map for dynamic part of query

--- www/folder_list.tcl
+++ www/folder_list.tcl
@@ -1,11 +1,13 @@
 set user_id [ad_conn user_id]
+set package_id [ad_conn package_id]
 
 if ![empty_string_p $file_id] {
-    set children_clause "and item_id not in (select item_id
-    from cr_items
-    where item_id != :file_id
-    connect by prior item_id = parent_id
-    start with item_id = :file_id)"
+    set children_clause [db_map children_clause]
+    #"and item_id not in (select item_id
+    #from cr_items
+    #where item_id != :file_id
+    #connect by prior item_id = parent_id
+    #start with item_id = :file_id)"
 } else {
     set children_clause ""
 }
@@ -14,15 +16,15 @@ if ![empty_string_p $file_id] {
 # Should base the choice on performance tests.
 
 set sql "
-select lpad('&nbsp;&nbsp;',12 * level,'&nbsp;&nbsp;') as spaces,
+ select lpad('&nbsp;&nbsp;',12 * level,'&nbsp;&nbsp;') as spaces,
      (select f.label from cr_folders f where f.folder_id = i.item_id) as label,
      (select f.folder_id from cr_folders f where f.folder_id = i.item_id) as new_parent
-from   cr_items i
-where  acs_permission.permission_p(i.item_id,:user_id,'write') = 't'
-and    exists (select 1 from cr_folders f where f.folder_id = i.item_id)
-$children_clause
-connect by prior item_id = parent_id
-start with item_id = file_storage.get_root_folder([ad_conn package_id])"
-
+ from   cr_items i
+ where  acs_permission.permission_p(i.item_id,:user_id,'write') = 't'
+ and    exists (select 1 from cr_folders f where f.folder_id = i.item_id)
+ $children_clause
+ connect by prior item_id = parent_id
+ start with item_id = file_storage.get_root_folder(:package_id)
+"
 db_multirow folder folder $sql
 
