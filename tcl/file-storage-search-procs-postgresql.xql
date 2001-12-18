7ea54e08fd1421d99decf63cac67e2a498877b64
Jowell Sabino <jowellsabino@netscape.net>
2001-12-18 20:56:35 +0000

    Site-wide search interface

---
+++ tcl/file-storage-search-procs-postgresql.xql
@@ -0,0 +1,47 @@
+<?xml version="1.0"?>
+
+<queryset>
+   <rdbms><type>postgresql</type><version>7.1</version></rdbms>
+
+
+<fullquery name="fs_datasource">
+      <querytext>
+	select r.revision_id as object_id,
+	       i.name as title,
+	       (case i.storage_type
+		     when 'lob' then r.lob::text
+		     when 'file' then '[cr_fs_path]' || r.content
+	             else r.content
+	        end) as content,
+	        r.mime_type as mime,
+	        '' as keywords,
+	        i.storage_type as storage_type
+	from cr_items i, cr_revisions r
+	where r.item_id = i.item_id
+	and   r.revision_id = :revision_id
+      </querytext>
+</fullquery>
+
+<fullquery name="fs_get_package_id">
+      <querytext>
+	select f.package_id as package_id
+	  from fs_root_folders f,
+       	  (select i2.parent_id
+	     from cr_items i1, cr_items i2, cr_revisions r
+	    where i1.item_id = r.item_id
+	      and r.revision_id = $revision_id
+              and i2.tree_sortkey <= i1.tree_sortkey
+	      and i1.tree_sortkey like (i2.tree_sortkey || '%')) as i
+  	  where f.folder_id = i.parent_id
+       </querytext>
+</fullquery>
+
+<fullquery name="fs_get_url_stub">
+      <querytext>
+        select site_node__url(node_id) as url_stub
+        from site_nodes
+        where object_id=:package_id
+      </querytext>
+</fullquery>
+
+</queryset> 
---
+++ tcl/file-storage-search-procs.tcl
@@ -0,0 +1,53 @@
+# Site-wide search procs
+ad_proc fs__datasource {
+    revision_id
+} {
+    @author Jowell S. Sabino (jowellsabino@netscape.net)
+} {
+    
+    db_0or1row fs_datasource "
+	select r.revision_id as object_id,
+	       i.name as title,
+	       case i.storage_type
+		     when 'lob' then r.lob::text
+		     when 'file' then '[cr_fs_path]' || r.content
+	             else r.content
+	        end as content,
+	        r.mime_type as mime,
+	        '' as keywords,
+	        i.storage_type as storage_type
+	from cr_items i, cr_revisions r
+	where r.item_id = i.item_id
+	and   r.revision_id = :revision_id
+    " -column_array datasource
+	
+    return [array get datasource]
+}
+	
+ad_proc fs__url {
+    revision_id
+} {
+    @author Jowell S. Sabino (jowellsabino@netscape.net)
+} {
+
+    db_1row fs_get_package_id "
+	select f.package_id as package_id
+	  from fs_root_folders f,
+       	  (select parent.parent_id
+	     from cr_items parent, cr_items children, cr_revisions r
+	    where children.item_id = r.item_id
+	      and r.revision_id = $revision_id
+              and children.tree_sortkey
+                    between parent.tree_sortkey 
+                    and     tree_right(parent.tree_sortkey) ) as i
+  	  where f.folder_id = i.parent_id
+    "
+
+    db_1row fs_get_url_stub "
+        select site_node__url(node_id) as url_stub
+        from site_nodes
+        where object_id=:package_id
+    "
+
+    return "${url_stub}download/index?version_id=$revision_id"
+}
