972e655086b55359d337501cba504ec55fdd2f6e
Dave Bauer <dave@thedesignexperience.org>
2004-03-31 02:35:32 +0000

    Add support for HEAD operation on webdav service contract.

--- tcl/file-storage-dav-install-procs.tcl
+++ tcl/file-storage-dav-install-procs.tcl
@@ -40,6 +40,7 @@ ad_proc -private fs::install::register_implementation {
         name "file_storage_object"
         aliases {
             get fs::impl::fs_object::get
+            head fs::impl::fs_object::head	    
             put fs::impl::fs_object::put
 	    propfind fs::impl::fs_object::propfind
 	    delete fs::impl::fs_object::delete
