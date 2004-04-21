02ed92c100e4efedda8093518e33f35e448accb4
Jeff Davis <davis@xarg.net>
2004-04-21 12:02:46 +0000

    add head lock and unlock methods to the file storage dav impl

--- tcl/file-storage-dav-procs.tcl
+++ tcl/file-storage-dav-procs.tcl
@@ -18,6 +18,24 @@ ad_proc fs::impl::fs_object::get {} {
     return [oacs_dav::impl::content_revision::get]
 }
 
+ad_proc fs::impl::fs_object::head {} {
+    HEAD method
+} {
+    return [oacs_dav::impl::content_revision::head]
+}
+
+ad_proc fs::impl::fs_object::lock {} {
+    LOCK method
+} {
+    return [oacs_dav::impl::content_revision::lock]
+}
+
+ad_proc fs::impl::fs_object::unlock {} {
+    UNLOCK method
+} {
+    return [oacs_dav::impl::content_revision::unlock]
+}
+
 ad_proc fs::impl::fs_object::put {} {
     PUT method
 } {
