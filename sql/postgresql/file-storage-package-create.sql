6ef2c2d9a3ed2222060a5debed563c6b65509400
Dave Bauer <dave@thedesignexperience.org>
2004-05-10 00:19:47 +0000

    Adding optional cascade_p parameter to file_storage__delete_folder to support deleting of folder contents.

--- sql/postgresql/file-storage-package-create.sql
+++ sql/postgresql/file-storage-package-create.sql
@@ -645,8 +645,28 @@ declare
         delete_folder__folder_id        alias for $1; 
 begin
 
+        return file_storage__delete_folder(
+                    delete_folder__folder_id,  -- folder_id
+                    'f'
+                    );
+
+end;' language 'plpgsql';
+
+create or replace function file_storage__delete_folder(
+       --
+       -- Delete a folder
+       --
+       integer,          -- cr_folders.folder_id%TYPE
+       boolean
+) returns integer as '  -- 0 for success
+declare
+        delete_folder__folder_id        alias for $1; 
+        delete_folder__cascade_p        alias for $2;
+begin
+
         return content_folder__delete(
-                    delete_folder__folder_id  -- folder_id
+                    delete_folder__folder_id,  -- folder_id
+                    delete_folder__cascade_p
                     );
 
 end;' language 'plpgsql';
