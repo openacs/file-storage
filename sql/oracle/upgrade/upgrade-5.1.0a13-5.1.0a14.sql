e8f2b7dd14474a27c1ca9342c4f8489123789f78
Jeff Davis <davis@xarg.net>
2005-02-24 13:33:19 +0000

    commit to HEAD of merge of changes on oacs-5-1 branch between tags jcd-merge-5-1-20050111 and jcd-merge-5-1-20050224

--- sql/oracle/upgrade/upgrade-5.1.0a13-5.1.0a14.sql
+++ sql/oracle/upgrade/upgrade-5.1.0a13-5.1.0a14.sql
@@ -9,4 +9,4 @@
 
 -- Add root folders into dav_site_node_folder_map
 
-insert into dav_site_node_folder_map (select sn.node_id, fs.folder_id, 'f' from fs_root_folders fs, site_nodes sn where sn.object_id=fs.package_id where fs.folder_id not in (select folder_id from dav_site_node_folder_map));
+insert into dav_site_node_folder_map (select sn.node_id, fs.folder_id, 'f' from fs_root_folders fs, site_nodes sn where sn.object_id=fs.package_id and fs.folder_id not in (select folder_id from dav_site_node_folder_map));
