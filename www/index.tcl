e17d4d3d684d174baca319f0db0e37b3374e48ea
Dario Roig <dario.roig@uv.es>
2005-07-01 14:03:02 +0000

    add regsub for escape $ to show url webdav

--- www/index.tcl
+++ www/index.tcl
@@ -106,6 +106,7 @@ set use_webdav_p  [ad_parameter "UseWebDavP"]
 
 if { $use_webdav_p == 1} { 
     set webdav_url [fs::webdav_url -item_id $folder_id]
+    regsub -all {/\$} $webdav_url {/\\$} webdav_url
 }
 
 # FIXME make this a parameter!
