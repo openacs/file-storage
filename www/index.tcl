d1e3fa2e3385bea527af314cacc1ec4458d5873c
Malte Sussdorff <openacs@sussdorff.de>
2005-08-25 21:05:45 +0000

    Added a backlink to the project. THis should be a callback, but I need this is a task for another day

--- www/index.tcl
+++ www/index.tcl
@@ -91,6 +91,29 @@ if {[form is_valid n_past_days_form]} {
 
 set context [fs_context_bar_list -root_folder_id $root_folder_id $folder_id]
 
+# Try to find a linked project so you can display a back link.
+# This should become a callback in the long run. 
+# For now I leave it in as it is.
+
+set project_item_id [application_data_link::get_linked -from_object_id $folder_id -to_object_type "content_item"]
+if {[exists_and_not_null project_item_id]} {
+    set project_url [pm::project::url -project_item_id $project_item_id]
+    set project_name [pm::project::name -project_item_id $project_item_id]
+} else {
+
+    # The folder itself was not linked. Let's try the parent folder.
+    set parent_folder [content::item::get_parent_folder -item_id $folder_id]
+    set project_item_id [application_data_link::get_linked -from_object_id $parent_folder -to_object_type "content_item"]
+    if {[exists_and_not_null project_item_id]} {
+	set project_url [pm::project::url -project_item_id $project_item_id]
+	set project_name [pm::project::name -project_item_id $project_item_id]
+    } else {
+	
+	# Neither this folder nor the parent folder was linked. Don't care...
+	set project_url {}
+    }
+}
+
 set up_url {}
 if { !${root_folder_p}} {
     if {[llength $context] == 1} {
