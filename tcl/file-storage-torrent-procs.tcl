1aa7eb0a7322e3d3e42151666cca9abc3cdfc360
Malte Sussdorff <openacs@sussdorff.de>
2005-10-29 13:52:51 +0000

    added file to store the torrent procedures.

---
+++ tcl/file-storage-torrent-procs.tcl
@@ -0,0 +1,22 @@
+ad_library {
+    @author Al-Faisal El-Dajani (faisal.dajani@gmail.com)
+    @creation-date 2005-10-25
+}
+
+namespace eval fs::torrent {}
+
+ad_proc -public fs::torrent::get_hashsum {
+    {-filename:required}
+} {
+    Get hashsum for the file using SHA1 hashsum technique.
+
+    @author Al-Faisal El-Dajani (faisal.dajani@gmail.com)
+    @creation-date 2005-10-25
+    @param filename Name of file to get hashsum for. Must be in absolute path format.
+    @return Hashsum of file in hexa.
+} {
+    set file_stream [open $filename r]
+    set file_contents [read $file_stream]
+    close $file_stream
+    return [ns_sha1 $file_contents]
+}
