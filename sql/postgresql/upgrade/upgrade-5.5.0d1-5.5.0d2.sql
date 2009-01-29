273642146ba15b57a77dbb2c8223d5dc406d0ee0
Emmanuelle Raffenne <eraffenne@gmail.com>
2009-01-29 14:31:57 +0000

    Fix param values for archive command and extension (were mixed up)

---
+++ sql/postgresql/upgrade/upgrade-5.5.0d1-5.5.0d2.sql
@@ -0,0 +1,7 @@
+-- fix param values (were mixed up in previous upgrade 5.4.0d1-5.4.0d2)
+
+update apm_parameters set default_value = 'zip' where parameter_name = 'ArchiveExtension' and package_key = 'file-storage';
+update apm_parameter_values set attr_value = 'zip' where parameter_id = (select parameter_id from apm_parameters where parameter_name = 'ArchiveExtension' and package_key = 'file-storage');
+
+update apm_parameters set default_value = '/usr/bin/zip -r {out_file} {in_file}' where parameter_name = 'ArchiveCommand' and package_key = 'file-storage';
+update apm_parameter_values set attr_value = '/usr/bin/zip -r {out_file} {in_file}' where parameter_id = (select parameter_id from apm_parameters where parameter_name = 'ArchiveCommand' and package_key = 'file-storage');
