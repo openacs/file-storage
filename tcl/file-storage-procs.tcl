1f2a485edaaa42f302d8d64316ad56d676778b7c
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2013-09-11 13:07:22 +0000

    - add missing nonpositional arg

--- tcl/file-storage-procs.tcl
+++ tcl/file-storage-procs.tcl
@@ -1160,7 +1160,7 @@ ad_proc fs::webdav_url {
 	if {[string equal $root_folder_id $item_id]} {
 	    set url_stub ""
 	} else {
-	    set url_stub [content::item::get_virtual_path -item_id $root_folder_id $item_id]
+	    set url_stub [content::item::get_virtual_path -root_folder_id $root_folder_id -item_id $item_id]
 	}
 	set package_url [apm_package_url_from_id $package_id]
 
