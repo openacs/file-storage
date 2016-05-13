fe6156cc736b90b7ee01817c71d5fa4904bf9938
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-05-13 07:06:24 +0000

    - strengthen page contract

--- www/file.tcl
+++ www/file.tcl
@@ -6,7 +6,7 @@ ad_page_contract {
     @cvs-id $Id$
 } {
     file_id:naturalnum,notnull
-    {show_all_versions_p:boolean "f"}
+    {show_all_versions_p:boolean,notnull "f"}
 } -validate {
     valid_file -requires {file_id} {
 	if {![fs_file_p $file_id]} {
