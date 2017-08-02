436ff00bf7fdf876d05409e5aa7c8c38b888edb7
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2017-08-02 07:36:06 +0000

    pass required return url to included adp

--- www/permissions.tcl
+++ www/permissions.tcl
@@ -23,6 +23,7 @@ if {[fs_file_p $object_id]} {
     set page_title [fs_get_folder_name $object_id]
     set context [fs_context_bar_list -final [_ acs-subsite.Permissions] -root_folder_id $root_folder_id $object_id]
 }
+set return_url [ad_return_url]
 # Local variables:
 #    mode: tcl
 #    tcl-indent-level: 4
