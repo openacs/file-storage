8ef0b54ebb85dfa95ae7ceef35af8230b9c91ecd
Antonio Pisano <antonio@elettrotecnica.it>
2018-07-16 15:52:44 +0000

    Redefine this page as ad_include_contract, therefore removing all hardcoded variable checks and exploit builtin checks, remove duplicated definitions and queries, streamline template::list definitions in a way its is readable and simpler to maintain

--- www/folder-chunk-postgresql.xql
+++ www/folder-chunk-postgresql.xql
@@ -1,9 +1,8 @@
 <?xml version="1.0"?>
-
 <queryset>
-   <rdbms><type>postgresql</type><version>7.1</version></rdbms>
-    <fullquery name="select_folder_contents">
-        <querytext>
+  <rdbms><type>postgresql</type><version>7.1</version></rdbms>
+  <fullquery name="select_folder_contents">
+    <querytext>
 
             select fs_objects.object_id,
                    fs_objects.mime_type,
@@ -23,68 +22,14 @@
                      then fs_objects.file_upload_name
                      else :folder_path || fs_objects.file_upload_name
                    end as file_url,
-                   case
-                     when fs_objects.last_modified >= (now() - cast('$n_past_days days' as interval))
-                     then 1
-                     else 0
-                   end as new_p
+                   fs_objects.last_modified >= (current_timestamp - cast('$n_past_days days' as interval)) as new_p
             from fs_objects
             where fs_objects.parent_id = :folder_id
 	    and   acs_permission__permission_p(fs_objects.object_id, :viewing_user_id, 'read')
 
             $categories_limitation
-	    $orderby
-        </querytext>
-    </fullquery>
+	    [template::list::orderby_clause -name contents -orderby]
 
-    <fullquery name="dbqd.file-storage.www.folder-chunk.select_folder_contents">
-        <rdbms><type>postgresql</type><version>8.4</version></rdbms>
-        <querytext>
-
-            select fs_objects.object_id,
-                   fs_objects.mime_type,
-                   fs_objects.name,
-                   fs_objects.live_revision,
-                   fs_objects.type,
-                   fs_objects.pretty_type,
-                   to_char(fs_objects.last_modified, 'YYYY-MM-DD HH24:MI:SS') as last_modified_ansi,
-                   fs_objects.content_size,
-                   fs_objects.url,
-                   fs_objects.sort_key,
-                   -fs_objects.sort_key as sort_key_desc,
-                   fs_objects.file_upload_name,
-                   fs_objects.title,
-                   case
-                     when :folder_path is null
-                     then fs_objects.file_upload_name
-                     else :folder_path || fs_objects.file_upload_name
-                   end as file_url,
-                   case
-                     when fs_objects.last_modified >= (now() - cast('$n_past_days days' as interval))
-                     then 1
-                     else 0
-                   end as new_p,
-                   case
-                    when fs_objects.type = 'folder' then
-                        (select description from cr_folders where folder_id = fs_objects.object_id)
-                    when fs_objects.type = 'url' then
-                        (select description from cr_extlinks where extlink_id = fs_objects.object_id)
-                   else
-                       (select description from cr_revisions where revision_id = fs_objects.live_revision)
-                   end as description
-            from fs_objects
-            where fs_objects.parent_id = :folder_id
-              and acs_permission__permission_p(fs_objects.object_id, :viewing_user_id,'read')
-        $orderby
-        </querytext>
-    </fullquery>
-
-    <fullquery name="get_folder_path">
-        <querytext>
-
-            select content_item__get_path(:folder_id, :root_folder_id);
-
-        </querytext>
-    </fullquery>
-
+    </querytext>
+  </fullquery>
 </queryset>
--- www/folder-chunk.tcl
+++ www/folder-chunk.tcl
@@ -1,11 +1,27 @@
 # file-storage/www/folder-chunk.tcl
 
-ad_page_contract {
+ad_include_contract {
     @author yon (yon@openforce.net)
     @creation-date Feb 22, 2002
     @cvs-id $Id$
-} -query {
-    {orderby:token,optional}
+} {
+    orderby:token,optional
+    {folder_id:integer ""}
+    {allow_bulk_actions:boolean true}
+    {category_id:integer ""}
+    {n_past_days:integer 99999}
+    {fs_url:localurl "[ad_conn package_url]"}
+    {return_url ""}
+    {format "table"}
+    {root_folder_id:integer ""}
+} -validate {
+    folder_id_exists {
+        if { $folder_id eq "" } {
+            ad_complain
+        }
+    }
+} -errors {
+    folder_id_exists [_ file-storage.lt_bad_folder_id_folder_]
 } -properties {
     folder_name:onevalue
     contents:multirow
@@ -13,16 +29,6 @@ ad_page_contract {
     page_num
 }
 
-if {![info exists folder_id] || $folder_id eq ""} {
-    ad_return_complaint 1 [_ file-storage.lt_bad_folder_id_folder_]
-    ad_script_abort
-}
-if {![info exists allow_bulk_actions] || $allow_bulk_actions eq ""} {
-    set allow_bulk_actions "0"
-}
-if { ![info exists category_id] } {
-    set category_id ""
-}
 set viewing_user_id [ad_conn user_id]
 
 permission::require_permission -party_id $viewing_user_id -object_id $folder_id -privilege "read"
@@ -38,32 +44,22 @@ if {!$delete_p} {
     set delete_p [permission::permission_p -party_id $viewing_user_id -object_id $folder_id -privilege "delete"]
 }
 
-if {![info exists n_past_days] || $n_past_days eq ""} {
-    set n_past_days 99999
-}
-
-if {![info exists fs_url] || $fs_url eq ""} {
-    set fs_url [ad_conn package_url]
-}
-
 set folder_name [lang::util::localize [fs::get_object_name -object_id  $folder_id]]
 
 set content_size_total 0
-
-if {![info exists format] || $format eq ""} {
-    set format table
-}
 
 #AG: We're an include file, and we may be included from outside file-storage.
 #So we need to query for the package_id rather than getting it from ad_conn.
 set package_and_root [fs::get_folder_package_and_root $folder_id]
 set package_id [lindex $package_and_root 0]
-if {(![info exists root_folder_id] || $root_folder_id eq "")} {
+if { $root_folder_id eq "" } {
     set root_folder_id [lindex $package_and_root 1]
 }
 
 if { $root_folder_id ne $folder_id } {
-    set folder_path "[db_exec_plsql get_folder_path {}]/"
+    set folder_path [content::item::get_path \
+                         -item_id $folder_id \
+                         -root_folder_id $root_folder_id]/
 } else {
     set folder_path ""
 }
@@ -134,61 +130,63 @@ if { $categories_p } {
 }
 
 #set n_past_filter_values [list [list "Yesterday" 1] [list [_ file-storage.last_week] 7] [list [_ file-storage.last_month] 30]]
-set elements [list \
-                  type \
-                  [list label [_ file-storage.Type] \
-                             display_template {<img src="@contents.icon@"  style="border: 0;" alt="@contents.alt_icon@" width="16" height="16">@contents.pretty_type@} \
-                             orderby_desc {sort_key_desc,fs_objects.pretty_type desc} \
-                             orderby_asc {fs_objects.sort_key, fs_objects.pretty_type asc}] \
-                  name \
-                  [list label [_ file-storage.Name] \
-                       display_template {<a @target_attr@ href="@contents.file_url@" title="\#file-storage.view_contents\#"><if @contents.title@ nil>@contents.name@</a></if><else>@contents.title@</a><br><if @contents.name@ ne @contents.title@>@contents.name@</if></else>} \
-                       orderby_desc {fs_objects.name desc} \
-                       orderby_asc {fs_objects.name asc}] \
-                  short_name \
-                  [list label [_ file-storage.Name] \
-                       hide_p 1 \
-                       display_template {<a href="@contents.download_url@" title="\#file-storage.Download\#">@contents.title@</a>} \
-                       orderby_desc {fs_objects.name desc} \
-                       orderby_asc {fs_objects.name asc}] \
-                  content_size_pretty \
-                  [list label [_ file-storage.Size] \
-                       display_template {@contents.content_size_pretty;noquote@} \
-                       orderby_desc {content_size desc} \
-                       orderby_asc {content_size asc}] \
-                  last_modified_pretty \
-                  [list label [_ file-storage.Last_Modified] \
-                       orderby_desc {last_modified_ansi desc} \
-                       orderby_asc {last_modified_ansi asc}] \
-                  properties_link \
-                  [list label "" \
-                       link_url_col properties_url \
-                       link_html { title "[_ file-storage.properties]" }] \
-                  new_version_link \
-                  [list label "" \
-                       link_url_col new_version_url \
-                       link_html { title "[_ file-storage.Upload_a_new_version]" }] \
-                  download_link \
-                  [list label "" \
-                       link_url_col download_url \
-                       link_html { title "[_ file-storage.Download]" }] \
-                 ]
-
+set elements {
+    type {
+        label #file-storage.Type#
+        display_template {<img src="@contents.icon@"  style="border: 0;" alt="@contents.alt_icon@" width="16" height="16">@contents.pretty_type@}
+    }
+    name {
+        label #file-storage.Name#
+        display_template {
+            <a @target_attr@ href="@contents.file_url@" title="#file-storage.view_contents#">
+            <if @contents.title@ nil>
+               @contents.name@</a>
+            </if>
+            <else>
+               @contents.title@</a><br>
+               <if @contents.name@ ne @contents.title@>
+                  @contents.name@
+               </if>
+            </else>
+        }
+    }
+    content_size_pretty {
+        label #file-storage.Size#
+        display_template {@contents.content_size_pretty;noquote@}
+    }
+    last_modified_pretty {
+        label #file-storage.Last_Modified#
+    }
+    properties_link {
+        label ""
+        link_url_col properties_url
+        link_html { title "#file-storage.properties#" }
+    }
+    new_version_link {
+        label ""
+        link_url_col new_version_url
+        link_html { title "#file-storage.Upload_a_new_version#" }
+    }
+    download_link {
+        label ""
+        link_url_col download_url
+        link_html { title "#file-storage.Download#" }
+    }
+}
 
 if { $categories_p } {
-    lappend elements categories [list label [_ file-storage.Categories] display_col "categories;noquote"]
+    lappend elements \
+        categories [list label [_ file-storage.Categories] display_col "categories;noquote"]
 }
-lappend elements views [list label "Views" ]
-
-
 
 if {[apm_package_installed_p views]} {
-    concat $elements [list views [list label "Views"]]
+    lappend elements views [list label "Views"]
 }
 
-if {![info exists return_url] || $return_url eq ""} {
+if { $return_url eq "" } {
     set return_url [export_vars -base "index" {folder_id}]
 }
+
 set vars_to_export [list return_url]
 
 
@@ -238,7 +236,7 @@ if {$format eq "list"} {
 }
 
 template::list::create \
-    -name contents_${folder_id} \
+    -name contents \
     -multirow contents \
     -key object_id \
     -actions $actions \
@@ -263,41 +261,62 @@ template::list::create \
         folder_id {hide_p 1}
         page_num
     } \
-    -elements $elements
+    -elements $elements \
+    -orderby {
+        default_value "name,asc"
+        type {
+            orderby_desc {fs_objects.sort_key desc, fs_objects.pretty_type desc}
+            orderby_asc {fs_objects.sort_key asc, fs_objects.pretty_type asc}
+        }
+        name {
+            orderby_desc {fs_objects.sort_key desc, fs_objects.name desc}
+            orderby_asc {fs_objects.sort_key asc, fs_objects.name asc}
+        }
+        content_size_pretty {
+            orderby_desc {content_size desc}
+            orderby_asc {content_size asc}
+        }
+        last_modified_pretty {
+            orderby_desc {last_modified_ansi desc}
+            orderby_asc {last_modified_ansi asc}
+        }
+    }
 
-set orderby [template::list::orderby_clause -orderby -name contents_${folder_id}]
+set categories_limitation [expr {$categories_p && $category_id ne "" ?
+                                 [db_map categories_limitation] : ""}]
 
-if {$orderby eq ""} {
-    set orderby " order by fs_objects.sort_key, fs_objects.name asc"
-}
+#"test [template::list::orderby_clause -name contents -orderby]"
 
-if { $categories_p && [info exists category_id] && $category_id ne "" } {
-    set categories_limitation [db_map categories_limitation]
-} else {
-    set categories_limitation {}
-}
-
-db_multirow \
-    -extend {label alt_icon icon last_modified_pretty content_size_pretty
-	properties_link properties_url download_link download_url
-	new_version_link new_version_url views categories
-    } contents select_folder_contents {} {
+db_multirow -extend {
+    label
+    alt_icon
+    icon
+    last_modified_pretty
+    content_size_pretty
+    properties_link
+    properties_url
+    download_link
+    download_url
+    new_version_link
+    new_version_url
+    views
+    categories
+} contents select_folder_contents {} {
     set last_modified_ansi [lc_time_system_to_conn $last_modified_ansi]
-
     set last_modified_pretty [lc_time_fmt $last_modified_ansi "%x %X"]
+
     if {$type eq "folder"} {
-        set content_size_pretty [lc_numeric $content_size]
-        append content_size_pretty "&nbsp;[_ file-storage.items]"
+        set content_size_pretty "[lc_numeric $content_size]&nbsp;[_ file-storage.items]"
         set pretty_type "#file-storage.Folder#"
     } else {
         if { $content_size eq "" } {
-            set content_size_pretty ""
-        } elseif {$content_size < 1024} {
+            set content_size 0
+        }
+        if {$content_size < 1024} {
             set content_size_pretty "[lc_numeric $content_size]&nbsp;[_ file-storage.bytes]"
         } else {
             set content_size_pretty "[lc_numeric [expr {$content_size / 1024 }]]&nbsp;[_ file-storage.kb]"
         }
-
     }
 
     if { $content_size ne "" } {
@@ -338,7 +357,6 @@ db_multirow \
             set file_url ${url}
             set download_url {}
             set download_link {}
-
         }
         symlink {
             set properties_link [_ file-storage.properties]
@@ -370,7 +388,6 @@ db_multirow \
                 } else {
                     set content_size_pretty "[lc_numeric [expr {$content_size / 1024 }]]&nbsp;[_ file-storage.kb]"
                 }
-
             }
             set properties_url [export_vars -base ${fs_url}file {{file_id $object_id}}]
             set new_version_link [_ acs-kernel.common_New]
@@ -405,7 +422,6 @@ db_multirow \
 		set file_url ${fs_url}view/${folder_path}[ad_urlencode_path $name]
             }
         }
-
     }
     if { $categories_p } {
         if { $type eq "folder" } {
@@ -421,10 +437,6 @@ db_multirow \
 			    -joinwith "<br>"]
     }
 
-}
-
-if { $expose_rss_p } {
-    db_multirow feeds select_subscrs {}
 }
 
 if {$format eq "list"} {
