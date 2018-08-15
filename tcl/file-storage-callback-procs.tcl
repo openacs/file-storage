53ab1afbc02371dad02eb9f59c664f2872b6826d
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-08-15 16:37:50 +0000

    Removed obsolete @arch-tag

--- tcl/file-storage-callback-procs.tcl
+++ tcl/file-storage-callback-procs.tcl
@@ -1,12 +1,9 @@
-## packages/file-storage/tcl/file-storage-callback-procs.tcl
-
 ad_library {
     
     Callback procs for file storage
     
     @author Malte Sussdorff (sussdorff@sussdorff.de)
     @creation-date 2005-06-15
-    @arch-tag: 921a2c2a-5593-495b-9a60-9d815d80a39d
     @cvs-id $Id$
 }
 
