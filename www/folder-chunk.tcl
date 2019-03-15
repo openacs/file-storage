c98a6f1b3cf8de00f6ae5ff9009eb8038b0c9ccd
Antonio Pisano <antonio@elettrotecnica.it>
2019-03-15 09:40:30 +0000

    Reduce divergency between Oracle and Postgres codebase

--- www/folder-chunk.tcl
+++ www/folder-chunk.tcl
@@ -304,7 +304,33 @@ db_multirow -extend {
     new_version_url
     views
     categories
-} contents select_folder_contents {} {
+} contents select_folder_contents [subst {
+            select fs_objects.object_id,
+                   fs_objects.mime_type,
+                   fs_objects.name,
+                   fs_objects.live_revision,
+                   fs_objects.type,
+                   fs_objects.pretty_type,
+                   to_char(fs_objects.last_modified, 'YYYY-MM-DD HH24:MI:SS') as last_modified_ansi,
+                   fs_objects.content_size,
+                   fs_objects.url,
+                   fs_objects.sort_key,
+                   -fs_objects.sort_key as sort_key_desc,
+                   fs_objects.file_upload_name,
+                   fs_objects.title,
+                   case
+                     when :folder_path is null
+                     then fs_objects.file_upload_name
+                     else :folder_path || fs_objects.file_upload_name
+                   end as file_url,
+                   fs_objects.last_modified >= (current_timestamp - cast('$n_past_days days' as interval)) as new_p
+            from fs_objects
+            where fs_objects.parent_id = :folder_id
+	    and   acs_permission.permission_p(fs_objects.object_id, :viewing_user_id, 'read')
+
+            $categories_limitation
+            $orderby
+}] {
     set last_modified_ansi [lc_time_system_to_conn $last_modified_ansi]
     set last_modified_pretty [lc_time_fmt $last_modified_ansi "%x %X"]
 
